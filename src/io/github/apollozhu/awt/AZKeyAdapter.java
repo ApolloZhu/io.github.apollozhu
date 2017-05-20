30f8a27503e39dfa92579ec7b9bdd57f0fcbfabe
ApolloZhu <public-apollonian@outlook.com>
2017-05-19 21:05:20 -0400

    Introduce AZKeyAdapter interface

---
+++ src/io/github/apollozhu/awt/AZKeyAdapter.java
@@ -0,0 +1,23 @@
+package io.github.apollozhu.awt;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public interface AZKeyAdapter extends KeyListener {
+	/**
+     * Invoked when a key has been typed.
+     * This event occurs when a key press is followed by a key release.
+     */
+    default void keyTyped(KeyEvent e) { }
+
+    /**
+     * Invoked when a key has been pressed.
+     */
+    default void keyPressed(KeyEvent e) { }
+
+    /**
+     * Invoked when a key has been released.
+     */
+    default void keyReleased(KeyEvent e) { }
+
+}
