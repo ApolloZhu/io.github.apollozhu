cd704d4d9268fbfde8d7ccaebb8b018d2634f826
ApolloZhu <public-apollonian@outlook.com>
2017-06-02 08:50:14 -0400

    [Beta] AZConsole
    Not tested yet

---
+++ src/io/github/apollozhu/AZConsole.java
@@ -0,0 +1,30 @@
+package io.github.apollozhu;
+
+public class AZConsole {
+	
+	public static void clear() {
+		System.out.print("\033[H\033[2J");
+	}
+
+	public static final String resetStyle = "\033[0m";
+
+	public static String bolded(String content) {
+		return format(content, 1);
+	}
+	
+	public static String italiclized(String content) {
+		return format(content, 3);
+	}
+	
+	public static String underlined(String content) {
+		return format(content, 4);
+	}
+	
+	public static String strikedThrough(String content) {
+		return format(content, 9);
+	}
+	
+	private static String format( String content, int code) {
+		return "\033[" + code + "m" + content;
+	}
+}
